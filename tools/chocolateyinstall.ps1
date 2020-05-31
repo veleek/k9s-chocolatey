2097bd2f20cc3916461638caa6fac87065ba145d
Ben Randall <veleek@gmail.com>
2020-05-30 22:44:44 -0700

    Better installer

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -3,30 +3,33 @@
 #   gc $f | ? {$_ -notmatch "^\s*#"} | % {$_ -replace '(^.*?)\s*?[^``]#.*','$1'} | Out-File $f+".~" -en utf8; mv -fo $f+".~" $f
 
 $ErrorActionPreference = 'Stop'; # stop on all errors
-$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-# Internal packages (organizations) or software that has redistribution rights (community repo)
-# - Use `Install-ChocolateyInstallPackage` instead of `Install-ChocolateyPackage`
-#   and put the binaries directly into the tools folder (we call it embedding)
-#$url        = '' # download url, HTTPS preferred
-$url64      = 'https://github.com/derailed/k9s/releases/download/v0.20.2/k9s_Windows_x86_64.tar.gz'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url64    = 'https://github.com/derailed/k9s/releases/download/v0.20.2/k9s_Windows_x86_64.tar.gz'
+$tarGzipName = (Split-Path -Leaf $url64)
+$tarGzipPath = Join-Path $toolsDir $tarGzipName
+$tarName  = [System.IO.Path]::GetFileNameWithoutExtension((Split-Path -Leaf $url64))
+$tarPath  = Join-Path $toolsDir $tarName
 
-$packageArgs = @{
-  packageName   = $env:ChocolateyPackageName
-  unzipLocation = $toolsDir
-  fileType      = 'exe'
-  url           = $url
-  url64bit      = $url64
-
-  softwareName  = 'k9s*' #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
-
-  # Checksums are now required as of 0.10.0.
-  # To determine checksums, you can get that from the original site if provided. 
-  # You can also use checksum.exe (choco install checksum) and use it 
-  # e.g. checksum -t sha256 -f path\to\file
-  #checksum      = ''
-  #checksumType  = 'sha256' #default is md5, can also be sha1, sha256 or sha512
-  checksum64    = '485ef75ccf8c4e5bac5f4595590816b9d3fc60cfb90fbf37c38a1847e28145ce'
-  checksumType64= 'sha256' #default is checksumType
+$getFileArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  fileFullPath64 = $tarGzipPath
+  url64bit       = $url64
+  checksum64     = '485ef75ccf8c4e5bac5f4595590816b9d3fc60cfb90fbf37c38a1847e28145ce'
+  checksumType64 = 'sha256'
 }
 
-Install-ChocolateyZipPackage @packageArgs
+$unGzipArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  fileFullPath64 = $tarGzipPath
+  destination    = $toolsDir
+}
+
+$unTarArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  fullFilePath   = $tarGzipPath
+  destination    = $toolsDir
+}
+
+Get-ChocolateyWebFile @getFileArgs
+Get-ChocolateyUnzip @unGzipArgs
+Get-ChocolateyUnzip @unTarArgs
